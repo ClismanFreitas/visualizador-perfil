73e20037ac83b3b8e9e32c514e839ff3c71905b7
Clisman Freitas <clismanfreitas.com@gmail.com>
2025-12-22 19:32:20 -0300

    feat: adicionado vereficação

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -18,8 +18,8 @@ export function renderProfile(userData, userRepos, container) {
     <div class="profile-card">
                 <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
                 <div class="profile-info">
-                    <h2> ${userData.name} </h2>
-                    <p>${userData.bio || "Não possui bio cadastrada"}</p>
+                    <h2> ${userData.name || "Não possui nome cadastrado😢"} </h2>
+                    <p>${userData.bio || "Não possui bio cadastrada😢"}</p>
                 </div>
             </div>
             
